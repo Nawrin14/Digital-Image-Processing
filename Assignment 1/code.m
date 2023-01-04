290b2bfe59cf979c47c8f870b7981696d2b536e0
Nawrin Tabassum <nawrintabassum14@gmail.com>
2023-01-04 13:40:53 +0600

    Add files via upload

---
+++ Assignment 1/code.m
@@ -0,0 +1,21 @@
+%Create the mirror image of an image and merge the two images
+
+I = imread('input.png');
+figure; 
+imshow(I);
+
+flipped = I(:, end:-1:1 ,:);
+figure;
+imshow(flipped);
+
+%original_grayscale = rgb2gray(I);
+%flipped_grayscale = rgb2gray(flipped);
+
+[row, col, dep] = size(I);
+merged = uint8(ones(row, col, dep));
+merged(:, 1:col, :) = I(:,:,:);
+merged(:, col+1:2*col, :) = flipped(:,:,:);
+figure; 
+imshow(merged);
+
+imwrite(merged, 'output.jpg');
